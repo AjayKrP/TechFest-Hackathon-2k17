08bed1ce22f275af960c24a03f84da719e8319a9
AjayKrP <ajayiiit8@gmail.com.com>
2017-08-26 16:32:23 +0530

    second

---
+++ js/effects.js
@@ -0,0 +1,105 @@
+let effects = ['fadeInDown', 'fadeInLeft', 'fadeInRight', 'fadeInUp'];
+var waypoint = new Waypoint({
+    element: document.getElementById('imageGallery'),
+    handler: function() {
+        let firstEffect = effects[1];
+        let secondEffect = effects[2];
+        $('.fil').addClass(firstEffect);
+        $('.fir').addClass(secondEffect);
+        setTimeout(()=>{
+            $('.fil').removeClass(firstEffect);
+            $('.fir').removeClass(secondEffect);
+        },2000);
+    }
+});
+
+
+var waypoint = new Waypoint({
+
+    element:document.getElementById('eventID'),
+    handler: function(direction) {
+        var className = "";
+        console.log(direction);
+        if(direction.toString() === 'down'){
+            $('.aboutL').addClass('fadeInLeft');
+            className = 'fadeInLeft';
+        }
+
+        else if(direction.toString() === 'up'){
+            $('.aboutL').addClass('fadeInRight');
+            className = 'fadeInRight';
+        }
+
+        console.log(className);
+        setTimeout(()=> {
+            $('.aboutL').removeClass(className);
+        },2000);
+    }
+});
+
+
+
+var waypoint = new Waypoint({
+    element:document.getElementById('LectureID'),
+    handler: function(direction) {
+        var className = "";
+        console.log(direction);
+        if(direction.toString() === 'down'){
+            $('.LecturesC').addClass('fadeInRight');
+            className = 'fadeInRight';
+        }
+
+        else if(direction.toString() === 'up'){
+            $('.LecturesC').addClass('fadeInLeft');
+            className = 'fadeInRight';
+        }
+
+        console.log(className);
+        setTimeout(()=> {
+            $('.LecturesC').removeClass(className);
+        },2000);
+    }
+});
+
+
+
+var waypoint = new Waypoint({
+    element:document.getElementById('eventID'),
+    handler: function(direction) {
+        var className = "";
+        console.log(direction);
+        if(direction.toString() === 'down'){
+            $('.eventC').addClass('fadeInLeft');
+            className = 'fadeInLeft';
+        }
+
+        else if(direction.toString() === 'up'){
+            $('.eventC').addClass('fadeInRight');
+            className = 'fadeInRight';
+        }
+
+        console.log(className);
+        setTimeout(()=> {
+            console.log()
+            $('.eventC').removeClass(className);
+        },2000);
+    },
+    offset: function() {
+        return Waypoint.viewportHeight() - this.element.clientHeight
+    }
+});
+
+
+
+/*
+var continuousElements = document.getElementsByClassName('row');
+for (var i = 0; i < continuousElements.length; i++) {
+    console.log(continuousElements[i]);
+    new Waypoint({
+        element: continuousElements[i],
+        handler: function() {
+            notify(this.element.innerHTML + ' hit')
+        }
+    })
+}
+*/
--- js/index.js
+++ js/index.js
@@ -18,3 +18,40 @@ $(document).ready(function(){
     }
     $(this).addClass("active");
 });
+
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+// init Masonry
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+});
+
+
+(function($) {
+    $(document).ready(function() {
+        $.jScrollability([
+            {
+                'selector': '.slide-in-demo',
+                'start': 'parent',
+                'end': 'parent',
+                'fn': {
+                    'left': {
+                        'start': 100,
+                        'end': 0,
+                        'unit': '%'
+                    }
+                }
+            }
+        ]);
+    });
+})(jQuery);
+
+
+
+
